26a07ddbe73b32d2b806b10be9bfb705faa0aaa9
Nicolas Arons <nicarons@gmail.com>
2019-04-24 14:34:23 -0400

    initial arduino code for controlling pneumatics

---
+++ Pneumatics/Pneumatics.ino
@@ -0,0 +1,124 @@
+//define pump motor things
+#define PUMPDIR 12 //pump is on terminals A
+#define PUMPPWM 3 //can use to moderate pressure
+#define PUMPCURRENT A0 //will help to tell torque and therefore pressure output
+#define PUMPBRAKE 9 //potentially useful? not sure. if the current is too high maybe.
+//define solenoid motor 
+#define SOLENOIDDIR 13 //solenoid is on terminals B
+#define SOLENOIDPWM 11 //this will be set to 255 or 0 always, nothing in between.
+#define SOLENOIDCURRENT A1 //not that useful, but we will keep it anyways
+#define SOLENOIDBRAKE 8 //also not useful
+/*
+penumatic connections:
+
+solenoid set up:        
+                              |
+                             solenoid 1
+|------------|  suck line     |
+|            |----------------|-------solenoid 2 ----|
+|   motor    |                                       -------output
+|            |----------------|-------solenoid 3 ----|
+|------------|  blow line    solenoid 4
+                              |
+
+blow is activated (inflating baloon) should be solenoid 4 + 2 are closed, 1+3 are energized. This corresponds to B- currently 
+(direction is LOW)
+suck is solenoid 2+4 are activated, which is B+ (direction HIGH)
+
+all solenoid black wires should go together on common ground (currently hooked up to the power supply)
+2+4 red go to B+
+1+3 red go to B-
+*/
+int pumping; //0 is nothing, 1 is suck, 2 is inflate, 3 is hold
+
+//these need to be set each loop, called in drive() function. set here to be able to store values for controls purposes (i think?)
+int pump_pwm;
+bool solenoid_pwm;//only ever all-on or all-off. no in between.
+bool solenoid_dir;
+
+int pump_current; //used to tell how hard the pump is driving
+
+void setup() {
+  // put your setup code here, to run once:
+  //Setup pinmodes for our pins
+  pinMode(PUMPDIR,OUTPUT);
+  pinMode(PUMPPWM,OUTPUT);
+  pinMode(PUMPCURRENT,INPUT);
+
+  pinMode(SOLENOIDDIR,OUTPUT);
+  pinMode(SOLENOIDPWM,OUTPUT);
+  pinMode(SOLENOIDCURRENT,INPUT);
+  //
+
+  digitalWrite(PUMPDIR,LOW);  //pump goes in the same direction reagrdless of polarity, so only PWM matters for it.
+
+
+
+  //begin serial communication
+  Serial.begin(9600); //different number might have to be used
+}
+
+void loop() {
+  get_pumping(); //use this to read value of pumping from serial connection. also will use to test :P
+  
+  // put your main code here, to run repeatedly:
+
+  //somehow read either a desired pressure or PWM or just being told to inflate
+  //ill assume that we are just being told to inflate, and that how much will be experimentally 
+  //decided and implemented by the arduino
+
+  //read values
+  pump_current = analogRead(PUMPCURRENT)/1.65;// Vout= 1.65V/A * current
+  //Serial.println(pump_current);
+  Serial.println(analogRead(SOLENOIDCURRENT)/1.65);
+  //somehow recieve value for pumping
+  if (pumping==1){ //suck
+    solenoid_dir=HIGH;
+    solenoid_pwm=HIGH;
+    pump_pwm=255;
+    //Serial.println("suck");
+  }else if (pumping ==2){ //blow
+    solenoid_dir=LOW;
+    solenoid_pwm=HIGH;
+    pump_pwm=255;
+    //Serial.println("blow");
+  }else if (pumping==3){ //hold
+    solenoid_dir=LOW;
+    solenoid_pwm=LOW;
+    pump_pwm=0;
+    //Serial.println("hold");
+  }else if (pumping==0){//no info
+    solenoid_dir=LOW;
+    solenoid_pwm=LOW;
+    pump_pwm=0;
+  } //if else, should do what was being done before. might have to change 0 to this, we shall see :P
+
+  drive();
+}
+
+void drive(){
+  //code to apply motor outputs to the actual motors/actuators
+  analogWrite(PUMPPWM,pump_pwm);
+  digitalWrite(SOLENOIDPWM,solenoid_pwm);
+  digitalWrite(SOLENOIDDIR,solenoid_dir);
+}
+
+void get_pumping(){ 
+  //eventually this should read off of the serial port or ros topic or however that thing works
+  
+  //below is the test code :P it alternates between sucking and blowing for testing purposes
+  int timed=millis();
+  //pumping=1;
+  //triple square wave/step wave:
+  
+  if ((timed%5000)<1000){
+    pumping=1; //suck
+  }else if (((timed%5000)>1000)&&((timed%5000)<4000)){
+    pumping=2; //blow
+  }else{
+    pumping=2;
+    //pumping=3; //hold
+  }
+}
+
+
